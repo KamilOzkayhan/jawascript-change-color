f8b4dda12cc9a91be7bd66290d6a12552c332ba3
Kamil Ozkayhan <ozkayhank@gmail.com>
2024-10-13 20:48:30 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function changeStyle() {
+
+    let body = document.getElementById('body');
+    let background = document.getElementById('background').value;
+    let color = document.getElementById('color').value;
+
+    body.style.background = background;
+    body.style.color = color;
+
+}
